a8f40a53caf27b129ca29badec752d2601f5f0cb
Lars Eckart <lars.eckart@gmail.com>
2020-02-14 09:18:04 +0200

    initial commit

---
+++ src/test/java/ee/lars/sprng/SprngApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.lars.sprng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

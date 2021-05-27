bebade26d873b15c5edbbec6ce9f98a38cbdd554
Lars Eckart <lars.eckart@hey.com>
2021-05-27 16:55:09 +0300

    F!! reset project

--- src/main/resources/insert-data.sql
+++
@@ -1,2 +0,0 @@
-insert  into `user`(`user_name`,`user_pass`,`enable`) values ('lars','1234',1);
-insert  into `user_role`(`user_name`,`user_role`) values ('lars','ROLE_ADMIN');
---
+++ src/test/java/com/larseckart/sprng/SprngApplicationTests.java
@@ -0,0 +1,15 @@
+package com.larseckart.sprng;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprngApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+}

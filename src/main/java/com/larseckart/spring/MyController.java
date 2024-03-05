56d707209ed742032292119a2037417c9627720b
Lars Eckart <lars.eckart@hey.com>
2024-03-05 18:32:26 +0200

    interact with database

--- src/main/java/com/larseckart/spring/MyController.java
+++ src/main/java/com/larseckart/spring/MyController.java
@@ -1,5 +1,7 @@
 package com.larseckart.spring;
 
+import java.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,16 @@ public class MyController {
   @Value("${mandatory.config.value}")
   String name;
 
+  @Autowired
+  private EventRepository repository;
+
   @RequestMapping("/")
   @ResponseBody
   String home() {
     if (!other.isEmpty()) {
       throw new IllegalStateException("optional config value will result in empty string");
     }
+    repository.save(new Event("Hello World!", LocalDateTime.now()));
     return "Hello World! " + name;
   }
 
